239e2b07bfcaa6c751852633fb3539180154fdb9
Alvaro <alvaro.regano@gmail.com>
2019-01-23 15:23:06 +0100

    padre service funcionando

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hijo } from '../models/Hijo';
+import { PadreService } from '../services/padre.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-padre',
@@ -8,26 +10,24 @@ import { Hijo } from '../models/Hijo';
 })
 export class PadreComponent implements OnInit {
 
-  hijos:Hijo[] = [
-    new Hijo(1, "Ricardo Jr", "Rodriguez", "2005-06-17", 105.6, "rjr@r.es", "rrrr"),
-    new Hijo(2, "Ricardo Jr", "Rodriguez", "2005-06-17", 105.6, "rjr@r.es", "rrrr"),];
-  valor:string='';
+  _hijos: Hijo[];
+  pid: number = 0;
 
-  constructor(private _hijoService:HijoService) { }
+  constructor(private route: ActivatedRoute, private _padreService: PadreService) { }
+
+  // ngOnInit() {
+  //   this._hijos = this._padreService.getHijos();
+  //   this.route.params.subscribe(params => {
+  //     console.log('params', params);
+  //     this.pid = params['pid'];
+  //   });
+  // }
 
   ngOnInit() {
-    this.hijos=this._hijoService;
+    this._padreService.getHijosByPidFromAPI(1).subscribe(hijosApi => {
+      console.log('hijosApi:',hijosApi);
+      this._hijos=hijosApi.hijos;
+    });
   }
-
-  filtraPorNombre(){
-    if(this.usuarios){
-      return this.usuarios.filter(unU=>{
-        return (unU.nombre.indexOf(this.valor)>=0);
-      });
-    }else{
-      return this.usuarios;
-    }
-  }
-
 
 }
