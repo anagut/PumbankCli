4c0c011e88cbeafce5262d87b8d885282de0d8f3
CarlosHernandezGonzalez <carlos.hgonzalez96@hotmail.com>
2019-01-24 17:56:02 +0100

    yo

--- src/botonborrar.component.ts
+++ src/botonborrar.component.ts
@@ -1,23 +1,23 @@
-// import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
-// import { Congelar } from 'src/app/models/Congelar';
+import { Congelar } from 'src/app/models/Congelar';
 
-// @Component({
-//   selector: 'boton-borrar',
-//   templateUrl: './botonborrar.component.html',
-// })
+@Component({
+  selector: 'boton-borrar',
+  templateUrl: './botonborrar.component.html',
+})
 
-// export class BotonBorrarComponent implements OnChanges{
-// 	@Input() cid: number;
-// 	@Output() borrarClicked: EventEmitter<string> = new EventEmitter<string>();
+export class BotonBorrarComponent implements OnChanges{
+	@Input() cid: number;
+	@Output() borrarClicked: EventEmitter<string> = new EventEmitter<string>();
 
-// 	ngOnChanges(sc:any): void {
-//     console.log('ngOnChanges',sc);
-//   }
+	ngOnChanges(sc:any): void {
+    console.log('ngOnChanges',sc);
+  }
 
-//   borra(cid:string) {
-//     console.log('Borrar CID:',cid);
-//     this.borrarClicked.emit(cid);
-//   }
+  borra(cid:string) {
+    console.log('Borrar CID:',cid);
+    this.borrarClicked.emit(cid);
+  }
 
-// }
+}
